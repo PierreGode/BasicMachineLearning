104ae4745f5b07043e39a83669bd73a1cd4bbdf9
PierreGode <8579922+PierreGode@users.noreply.github.com>
2024-01-10 23:53:04 +0100

    Update saldo.py

--- saldo.py
+++ saldo.py
@@ -1,7 +1,7 @@
 import csv
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Linear Regression
+from sklearn.linear_model import LinearRegression  # Corrected import statement
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import calendar
